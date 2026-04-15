97a92d7971eb6424e89ceb1074b33778342e5ad2
zhuxiaokang <zhu930824@outlook.com>
2026-04-15 14:29:18 +0800

    feat: 完善功能

--- rag-backend/src/main/java/cn/sdh/backend/entity/KnowledgeDocument.java
+++ rag-backend/src/main/java/cn/sdh/backend/entity/KnowledgeDocument.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -59,8 +60,10 @@ public class KnowledgeDocument implements Serializable {
     private Integer status;
 
     @TableField("create_time")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime createTime;
 
     @TableField("update_time")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime updateTime;
 }
